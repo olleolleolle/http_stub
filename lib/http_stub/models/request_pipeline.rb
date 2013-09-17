bed63d5a57279de81d1a2c8956ba6a8a81a7acaf
dueckes <matthew@ueckerman.net>
2013-09-17 13:02:57 +1000

    delay is always established and defaults to 0

--- lib/http_stub/models/request_pipeline.rb
+++ lib/http_stub/models/request_pipeline.rb
@@ -1,10 +1,13 @@
 module HttpStub
   module Models
+
     class RequestPipeline
 
       def self.before_halt(response)
-        sleep response.delay_in_seconds if response.delay_in_seconds
+        sleep response.delay_in_seconds
       end
+
     end
+
   end
 end
